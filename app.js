4720c28e86da5c06ca52a852af43540b03e33df8
alisamfp <eatliightning@gmail.com>
2014-10-05 20:43:23 -0700

    added images and tab support

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 (function(){
-  var app = angular.module('store', [ ]);
+  var app = angular.module('gemStore', [ ]);
   
   app.controller('StoreController', function(){
     this.products = gems;
   });
 
-  var gems = [
-  {
-    name: 'Dedecahedron',
-    price: 2,
-    description: '. . .',
+  var gems = [{
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
     images: [
-      {
-        full: 'dodecahedron-01-full.jpg',
-        thumb: 'dodecahedron-01-thumb.jpg'
-      },
-      {
-        full: 'dodecahedron-02-full.jpg',
-        thumb: 'dodecahedron-02-thumb.jpg'
-      }
+      "images/gem-02.gif",
+      "images/gem-05.gif",
+      "images/gem-09.gif"
     ]
-  },
-  {
-    name: "Pentagonal Gem",
-    price: 5.95,
-    description: '........',
+  }, {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
     images: [
-      {
-        full: 'pentagonal-01-full.jpg',
-        thumb: 'pentagonal-01-thumb.jpg'
-      },
-      {
-        full: 'pentagonal-02-full.jpg',
-        thumb: 'pentagonal-02-thumb.jpg'
-      }
+      "images/gem-01.gif",
+      "images/gem-03.gif",
+      "images/gem-04.gif"
+    ]
+  }, {
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
+    images: [
+      "images/gem-06.gif",
+      "images/gem-07.gif",
+      "images/gem-08.gif"
     ]
   }];
 })();
