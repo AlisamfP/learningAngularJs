063b1ea7c03f5da4f82d4778ed77b55c8fad6d3e
alisamfp <eatliightning@gmail.com>
2014-10-05 20:08:46 -0700

    adding tabs

--- app.js
+++ app.js
@@ -10,12 +10,30 @@
     name: 'Dedecahedron',
     price: 2,
     description: '. . .',
-    canPurchase: true
+    images: [
+      {
+        full: 'dodecahedron-01-full.jpg',
+        thumb: 'dodecahedron-01-thumb.jpg'
+      },
+      {
+        full: 'dodecahedron-02-full.jpg',
+        thumb: 'dodecahedron-02-thumb.jpg'
+      }
+    ]
   },
   {
     name: "Pentagonal Gem",
     price: 5.95,
     description: '........',
-    canPurchase: true
+    images: [
+      {
+        full: 'pentagonal-01-full.jpg',
+        thumb: 'pentagonal-01-thumb.jpg'
+      },
+      {
+        full: 'pentagonal-02-full.jpg',
+        thumb: 'pentagonal-02-thumb.jpg'
+      }
+    ]
   }];
 })();
